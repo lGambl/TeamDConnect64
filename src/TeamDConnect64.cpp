42f8fba22879e2748074a5905e94c3a2be20f23e
StevenKight <sgk0711@gmail.com>
2023-04-11 20:06:39 -0400

    Create base c++ project.

---
+++ src/TeamDConnect64.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TeamDConnect64.cpp
+// Author      : Team D
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Hello World!!!" << endl;
+
+    return 0;
+}
