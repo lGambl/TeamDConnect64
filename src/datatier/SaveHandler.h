dda4a92cc16c719a3062baab10ec9fdfa7199fff
ljones48 <ljones48@my.westga.edu>
2023-04-22 13:37:18 -0400

    Update documentation for save handler

--- src/datatier/SaveHandler.h
+++ src/datatier/SaveHandler.h
@@ -23,13 +23,44 @@ using namespace datatier;
 
 namespace datatier {
 
+/**
+ *  A save handler class responsible for handling saves
+ */
 class SaveHandler {
 
 public:
+	/**
+	 * Initializes a save handler
+	 */
 	SaveHandler();
+
+	/**
+	 * Deconstructs a save handler
+	 */
 	virtual ~SaveHandler();
+
+	/**
+	 * Load a save given a file name
+	 *
+	 * @param fileName
+	 *
+	 * @return nodes of the board
+	 */
 	vector<Node*> loadSave(string &fileName);
+
+	/**
+	 * Save a game to a give file
+	 *
+	 * @param fileName
+	 * @param nodes
+	 */
 	void saveGame(string &fileName, vector<Node*> nodes);
+
+	/**
+	 * Get saves that are in the main directory of the project
+	 *
+	 * @return a vector of all save names
+	 */
 	vector<string> getSaves();
 };
 
